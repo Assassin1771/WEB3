b716820260aed98e871c97fe9c84dc82ec046ac4
Tushar Garg <tshrgarg2010@gmail.com>
2022-05-02 11:22:19 +0530

    Basic Files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// import {
+//   Navbar,
+//   Footer,
+//   Loader,
+//   Services,
+//   Transactions,
+//   Welcome,
+// } from "./components/index";
+
+const App = () => <div>HELLO WORLD</div>;
+
+export default App;
